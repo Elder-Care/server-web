fe6acb5963c981920ceac9a0429c1e2fd595ee73
w294912757 <6503963+w294912757@user.noreply.gitee.com>
2020-07-06 16:19:09 +0800

    路由测试

--- app.py
+++ app.py
@@ -470,7 +470,7 @@ def addo():
     form = request.form
     if not form.get('username'):
         content = "请输入用户名"
-        return render_template('addo.html', content=content)
+        return render_template('/htmls/add_old.html', content=content)
     info = {'username': form.get('username'), 'gender': form.get('gender'), 'phone': form.get('phone'),
             'id_card': form.get('id_card'),
             'birthday': form.get('birthday'), 'checkin_date': form.get("checkin_date"),
@@ -486,10 +486,10 @@ def addo():
     global conn
     if add_elder(conn, info):
         content = "增添成功"
-        return render_template('addo.html', content=content)
+        return render_template('/htmls/index.html', content=content)
     else:
         content = "用户名已存在"
-        return render_template('addo.html', content=content)
+        return render_template('/htmls/index.html', content=content)
 
 
 @app.route('/oinfo', methods=['POST', 'GET'])
